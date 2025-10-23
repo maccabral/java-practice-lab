63bae9cc47e4b4c2e181a7fe647e2d4b3822f7e3
maccabral <jmbulaycabral@gmail.com>
2025-10-24 00:24:16 +0800

    trycatch

--- P1_ExceptionHandlingTryCatchFinally/src/p1main/Main.java
+++ P1_ExceptionHandlingTryCatchFinally/src/p1main/Main.java
@@ -1,9 +1,36 @@
 package p1main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		Example - #1
+//		Scanner s = new Scanner(System.in);
+//		try {
+//			System.out.print("Number: ");
+//			int x = s.nextInt();
+//			
+//			System.out.println("Correct! " + x);
+//		}catch(Exception e) {
+//			System.out.println("Datatype error!");
+//		} finally {
+//			s.close();
+//		}
+		
+//		Example - #2
+		String names[] = {"Sachie", "Batutilan"};
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Index: ");
+		
+		int index = s.nextInt();
+		
+		System.out.println(names[index]);
+		
+		
 
 	}
 
